d37bb532761bdc68f0fe2196f6c1a61ce59a21b0
sudheergodugu <sudheergodugu@gmail.com>
2017-02-04 18:25:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req,res) {
     res.send ('article one requested and served here');
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
