dada0a8dec77c1db308430070c67ca7d92946a07
sudheergodugu <sudheergodugu@gmail.com>
2017-02-10 16:49:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,3 @@
 console.log('Loaded!');
+script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
+<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
