c5323e73c5563f5ce8e4fa34b03e3ae2432f880c
sudheergodugu <sudheergodugu@gmail.com>
2017-03-11 19:42:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ var express = require('express');
  app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
- 
+ app.get('/article-one', function (req, res) {
+   res.send('article one requested and served here'));
+ });
+ app.get('/article-two', function (req, res) {
+   res.send('article two requested and served here'));
+ });
  app.get('/ui/style.css', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
  });
@@ -17,9 +22,7 @@ var express = require('express');
    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
  });
  
-  app.get('/article-one', function (req, res) {
-   res.send('article one requested and served here'));
- });
+  
  app.get('/ui/madi.png', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
  });
