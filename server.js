4f7b69e052fd3d651e5a49b7a83e247b53a9a518
sudheergodugu <sudheergodugu@gmail.com>
2017-03-11 13:44:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'sudheergodugu',
-    database: 'sudheergodugu',
+    user: 'darkfist',
+    database: 'darkfist',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
@@ -174,12 +174,7 @@ app.get('/articles/:articleName', function (req, res) {
   });
   
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
