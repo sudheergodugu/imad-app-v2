a216ae3673ce2a8caa7258b2e1cb8aba223c726e
sudheergodugu <sudheergodugu@gmail.com>
2017-03-11 19:48:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,26 +6,18 @@ var express = require('express');
  app.use(morgan('combined'));
  
  app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
- });
- app.get('/article-one', function (req, res) {
-   res.send('article one requested and served here'));
- });
- app.get('/article-two', function (req, res) {
-   res.send('article two requested and served here'));
- });
- app.get('/ui/style.css', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
  
+ +app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
  app.get('/ui/madi.png', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
  });
  
-  
- app.get('/ui/madi.png', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
- });
+ 
  var port = 8080; // Use 8080 for local development because you might already have apache running on 80
  app.listen(8080, function () {
    console.log(`IMAD course app listening on port ${port}!`);
